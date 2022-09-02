d4688c136187beb39be3ff74005626d24d1ca590
Valentino <valentino.giudice@network.elancoah.com>
2022-09-02 15:17:24 +0200

    Add todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
+import {
+  useRef,
+  useState,
+  FormEvent,
+  ChangeEvent,
+  MutableRefObject,
+} from "react";
+
+type Todo = { id: number; name: string };
+
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState<Todo>();
+
+  const input = useRef() as MutableRefObject<HTMLInputElement>;
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewTodo({ id: todos.length + 1, name: e.target.value });
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (newTodo) {
+      setTodos((todos) => [newTodo, ...todos]);
+      input.current.value = "";
+    }
+  };
+
   return (
     <div>
       <h1>Todo App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          placeholder="Add Todo"
+          ref={input}
+          type="text"
+          name="todo"
+        />
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
